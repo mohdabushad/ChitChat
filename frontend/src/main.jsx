37c3a82788c3cb866591b9445a03277822caab29
Muhammad Abushad <mmohammadabushad@gmail.com>
2026-02-12 13:43:56 +0530

    Switch to HashRouter for reload-safe routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-
-import { store } from "./store/store.js";
 import { Provider } from "react-redux";
+import store from "./store/store"; // अगर store है
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 );
