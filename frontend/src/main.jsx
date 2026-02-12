38abb4bfa6010e45aa7db17a074b794cbdae46cb
Muhammad Abushad <mmohammadabushad@gmail.com>
2026-02-12 13:57:36 +0530

    Fix Redux store export and use HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "./store/store";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
+
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </HashRouter>
+
 );
