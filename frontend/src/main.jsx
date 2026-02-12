4cb4a3e9c22fbc26742e38cc2b9e1fbb6888c89c
Muhammad Abushad <mmohammadabushad@gmail.com>
2026-02-12 13:54:20 +0530

    Enable clean URL routing on Render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import App from "./App";
-
+import store from "./store/store"; // अगर store है
 
 createRoot(document.getElementById("root")).render(
-
-    <BrowserRouter>
+  <Provider store={store}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-
+    </HashRouter>
+  </Provider>,
 );
